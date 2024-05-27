17924d3653c0d5b161255ada54d207bf736b2c9b
Andres Mendoza Ramirez <131287761+andremendoza1@users.noreply.github.com>
2024-05-27 09:07:06 -0600

    Formulario

---
+++ 03Ejercicios/JS/inversion.js
@@ -0,0 +1,26 @@
+
+
+function validarn(e){
+    var teclado = (document.all)?e.keycode : e.which;
+    if(teclado == 8) return true;
+    var patron = /[0-9\d .]/;
+    var codigo = String.fromCharCode(teclado);
+    return patron.test(codigo);
+}
+
+function interes(){
+    var valor = document.formulario.cantidad.value;
+
+    var resultado = parseInt(valor);
+    var interes = resultado * 0.15;
+    var total = resultado + interes;
+
+
+    document.formulario.sueldoI.value = "$"+total;
+
+}
+
+function borrar(){
+    document.formulario.sueldoI.value="";
+    document.formulario.cantidad.value="";
+}
