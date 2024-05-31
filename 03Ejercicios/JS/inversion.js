1c5029822a63cabc7be43782e3a1097391bbf0f9
Andres Mendoza Ramirez <131287761+andremendoza1@users.noreply.github.com>
2024-05-31 09:16:10 -0600

    6 EJrcicios de Tarea

--- 03Ejercicios/JS/inversion.js
+++ 03Ejercicios/JS/inversion.js
@@ -1,7 +1,10 @@
-
+//como calculamos la inversion al 15% anual
+//una cantidad x  por ejemplo 1000 si los 1000 estan por un año se aplica el 15%
+// 1000 * 15% = interes que gane
+// 1000 + interes = resultado final
 
 function validarn(e){
-    var teclado = (document.all)?e.keycode : e.which;
+    var teclado = (document.all)?e.keyCode : e.which;
     if(teclado == 8) return true;
     var patron = /[0-9\d .]/;
     var codigo = String.fromCharCode(teclado);
@@ -10,17 +13,17 @@ function validarn(e){
 
 function interes(){
     var valor = document.formulario.cantidad.value;
+    //ese valor no es numero recordando que es un lenguaje no typado
+    //debemos de parsearlo a int
+    var result = parseInt(valor);
+    var interes = result * 0.15;
+    var total = result + interes;
 
-    var resultado = parseInt(valor);
-    var interes = resultado * 0.15;
-    var total = resultado + interes;
-
-
-    document.formulario.sueldoI.value = "$$"+total;
-
+    //imprimimos en el html
+    document.formulario.sueldoI.value = "$"+total;
 }
 
-function borrar(){
+function borrarn(){
     document.formulario.sueldoI.value="";
     document.formulario.cantidad.value="";
 }
